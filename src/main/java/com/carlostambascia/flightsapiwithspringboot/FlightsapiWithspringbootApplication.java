e58de42ccfeb637c322f9c9458bf2b2a7e835910
Carlos Tambascia <carlos.tambascia@gmail.com>
2021-03-21 19:06:43 -0300

    first commit

---
+++ src/main/java/com/carlostambascia/flightsapiwithspringboot/FlightsapiWithspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.carlostambascia.flightsapiwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsapiWithspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightsapiWithspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlostambascia/flightsapiwithspringboot/FlightsapiWithspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlostambascia.flightsapiwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsapiWithspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
